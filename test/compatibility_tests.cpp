15dc89ef791e37e5d6075f2359190b92c27be9c3
KakenHe <asa@kakenhe.com>
2026-07-18 22:27:31 +0800

    test: lock IPv6 and user script compatibility
    The compatibility suite passes against the pre-migration QtScript implementation on Fedora 43, including the focused IPv6 golden and user-script behavior cases.

--- test/compatibility_tests.cpp
+++ test/compatibility_tests.cpp
@@ -1,15 +1,19 @@
 #include "crc32c.h"
 #include "eth2.pb.h"
 #include "ip4.pb.h"
+#include "ip6.pb.h"
 #include "mac.pb.h"
 #include "ostmfileformat.h"
 #include "ossnfileformat.h"
 #include "payload.pb.h"
 #include "pbrpccommon.h"
 #include "protocolmanager.h"
+#include "protocollistiterator.h"
 #include "streambase.h"
 #include "tcp.pb.h"
 #include "udp.pb.h"
+#include "userscript.h"
+#include "userscript.pb.h"
 #include "vlan.pb.h"
 
 #include <QFile>
@@ -111,6 +115,86 @@ QByteArray packetBytes(bool tcp, bool vlan = false)
     return bytes;
 }
 
+QByteArray ipv6UdpPacketBytes()
+{
+    OstProto::Stream config;
+    config.mutable_stream_id()->set_id(2);
+    config.mutable_core()->set_frame_len(82);
+
+    OstProto::Mac *mac = addProtocol(
+        config, OstProto::Protocol::kMacFieldNumber)->MutableExtension(
+            OstProto::mac);
+    mac->set_dst_mac_mode(OstProto::Mac::e_mm_fixed);
+    mac->set_dst_mac(0x001122334455ULL);
+    mac->set_src_mac_mode(OstProto::Mac::e_mm_fixed);
+    mac->set_src_mac(0x66778899aabbULL);
+    addProtocol(config, OstProto::Protocol::kEth2FieldNumber)
+        ->MutableExtension(OstProto::eth2);
+
+    OstProto::Ip6 *ip = addProtocol(
+        config, OstProto::Protocol::kIp6FieldNumber)->MutableExtension(
+            OstProto::ip6);
+    ip->set_hop_limit(64);
+    ip->set_src_addr_hi(0x20010db800000000ULL);
+    ip->set_src_addr_lo(1);
+    ip->set_dst_addr_hi(0x20010db800000000ULL);
+    ip->set_dst_addr_lo(2);
+
+    OstProto::Udp *udp = addProtocol(
+        config, OstProto::Protocol::kUdpFieldNumber)->MutableExtension(
+            OstProto::udp);
+    udp->set_is_override_src_port(true);
+    udp->set_is_override_dst_port(true);
+    udp->set_src_port(12345);
+    udp->set_dst_port(53);
+
+    OstProto::Payload *payload = addProtocol(
+        config, OstProto::Protocol::kPayloadFieldNumber)->MutableExtension(
+            OstProto::payload);
+    payload->set_pattern_mode(OstProto::Payload::e_dp_fixed_word);
+    payload->set_pattern(0x01020304);
+
+    StreamBase stream;
+    stream.protoDataCopyFrom(config);
+    QByteArray bytes(78, 0);
+    const int size = stream.frameValue(
+        reinterpret_cast<uchar *>(bytes.data()), bytes.size(), 0);
+    if (size != bytes.size())
+        return QByteArray();
+    return bytes;
+}
+
+UserScriptProtocol *userScriptProtocol(StreamBase &stream)
+{
+    ProtocolListIterator *iterator = stream.createProtocolListIterator();
+    UserScriptProtocol *result = nullptr;
+    while (iterator->hasNext()) {
+        AbstractProtocol *protocol = iterator->next();
+        if (protocol->protocolNumber()
+                == OstProto::Protocol::kUserScriptFieldNumber) {
+            result = static_cast<UserScriptProtocol *>(protocol);
+            break;
+        }
+    }
+    delete iterator;
+    return result;
+}
+
+UserScriptProtocol *loadUserScript(StreamBase &stream, const QString &program)
+{
+    OstProto::Stream config;
+    config.mutable_stream_id()->set_id(3);
+    config.mutable_core()->set_frame_len(64);
+    OstProto::UserScript *script = addProtocol(
+        config, OstProto::Protocol::kUserScriptFieldNumber)->MutableExtension(
+            OstProto::userScript);
+    script->set_program(program.toStdString());
+    addProtocol(config, OstProto::Protocol::kPayloadFieldNumber)
+        ->MutableExtension(OstProto::payload);
+    stream.protoDataCopyFrom(config);
+    return userScriptProtocol(stream);
+}
+
 QByteArray rpcFrame(quint16 type, quint16 method, const QByteArray &payload)
 {
     QByteArray frame(PB_HDR_SIZE, 0);
@@ -158,6 +242,9 @@ private slots:
     void ethernetIpv4UdpGolden();
     void ethernetIpv4TcpGolden();
     void vlanIpv4UdpGolden();
+    void ethernetIpv6UdpGolden();
+    void userScriptHostApiAndByteConversion();
+    void userScriptRuntimeException();
     void nativeStreamsRoundTripAndValidation();
     void nativeSessionRoundTrip();
     void rpcHeaderGolden();
@@ -207,6 +294,66 @@ void CompatibilityTests::vlanIpv4UdpGolden()
         "c0000201c6336402303900350016d6080102030401020304010203040102"));
     QCOMPARE(actual.mid(28, 2).toHex(), QByteArray("7c58"));
     QCOMPARE(actual.mid(44, 2).toHex(), QByteArray("d608"));
+}
+
+void CompatibilityTests::ethernetIpv6UdpGolden()
+{
+    const QByteArray actual = ipv6UdpPacketBytes();
+    QCOMPARE(actual.toHex(), QByteArray(
+        "00112233445566778899aabb86dd600000000018114020010db800000000"
+        "000000000000000120010db8000000000000000000000002303900350018"
+        "63c301020304010203040102030401020304"));
+    QCOMPARE(actual.mid(18, 2).toHex(), QByteArray("0018"));
+    QCOMPARE(actual.mid(60, 2).toHex(), QByteArray("63c3"));
+}
+
+void CompatibilityTests::userScriptHostApiAndByteConversion()
+{
+    StreamBase stream;
+    UserScriptProtocol *script = loadUserScript(stream, QString::fromLatin1(
+        "protocol.name = 'Baseline';\n"
+        "protocol.protocolFrameSizeVariable = true;\n"
+        "protocol.protocolFrameVariableCount = 3;\n"
+        "protocol.protocolFrameValue = function(i) {\n"
+        "  return [i, 255, 256, -1, 1.9, '2'];\n"
+        "};\n"
+        "protocol.protocolFrameSize = function(i) { return 6 + i; };\n"
+        "protocol.protocolId = function(type) {\n"
+        "  return type == Protocol.ProtocolIdEth ? 0x88b5 : 0;\n"
+        "};"));
+
+    QVERIFY(script);
+    QVERIFY2(script->isScriptValid(), qPrintable(script->userScriptErrorText()));
+    QCOMPARE(script->name(), QString("Baseline:{UserScript} [EXPERIMENTAL]"));
+    QVERIFY(script->isProtocolFrameSizeVariable());
+    QCOMPARE(script->protocolFrameVariableCount(), 3);
+    QCOMPARE(script->protocolFrameSize(2), 8);
+    QCOMPARE(script->protocolId(AbstractProtocol::ProtocolIdEth),
+             quint32(0x88b5));
+    QCOMPARE(script->fieldData(UserScriptProtocol::userScript_program,
+                               AbstractProtocol::FieldFrameValue, 2)
+                 .toByteArray().toHex(),
+             QByteArray("02ff00ff0102"));
+}
+
+void CompatibilityTests::userScriptRuntimeException()
+{
+    StreamBase stream;
+    UserScriptProtocol *script = loadUserScript(stream, QString::fromLatin1(
+        "protocol.protocolFrameValue = function(i) {\n"
+        "  throw new Error('baseline failure');\n"
+        "};\n"
+        "protocol.protocolFrameSize = function(i) { return 0; };"));
+
+    QVERIFY(script);
+    QVERIFY(!script->isScriptValid());
+    QCOMPARE(script->userScriptErrorLineNumber(), 2);
+    QCOMPARE(script->userScriptErrorText(), QString("Error: baseline failure"));
+    QCOMPARE(script->protocolFrameSize(), 0);
+    QCOMPARE(script->fieldData(UserScriptProtocol::userScript_program,
+                               AbstractProtocol::FieldFrameValue)
+                 .toByteArray(),
+             QByteArray());
 }
 
 void CompatibilityTests::nativeStreamsRoundTripAndValidation()
